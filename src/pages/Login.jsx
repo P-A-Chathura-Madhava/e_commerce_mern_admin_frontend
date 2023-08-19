c9035d06209d4b6a5e059cbccf8659256992245d
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-19 09:52:14 +0530

    implement login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import CustomInput from "../components/CustomInput";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-3">
+        <h3 className="text-center">Login</h3>
+        <p className="text-center">Login to your account to continue.</p>
+        <form action="">
+          <CustomInput type="text" label="Email Address" id="email" />
+          <CustomInput type="password" label="Password" id="pass" />
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
