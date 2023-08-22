77d4de941c6a729af5afcc286882511aa68bd8fd
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-22 17:54:34 +0530

    implement color list API integration

---
+++ src/features/color/colorSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import colorService from "./colorService";
+
+export const getColors = createAsyncThunk(
+  "color/get-colors",
+  async (thunkAPI) => {
+    try {
+      return await colorService.getColors();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const initialState = {
+  colors: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+export const colorSlice = createSlice({
+  name: "colors",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getColors.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getColors.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.colors = action.payload;
+      })
+      .addCase(getColors.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      });
+  },
+});
+export default colorSlice.reducer;
