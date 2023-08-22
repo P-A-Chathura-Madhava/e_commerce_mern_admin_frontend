b2b992c1111638bd50d93afa3e619a62652fb1e1
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-22 18:52:51 +0530

    implement blog category list API integration

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,8 @@ import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
 import brandReducer from "../features/brand/brandSlice";
 import pCategoryReducer from "../features/pcategory/pcategorySlice";
+import bCategoryReducer from "../features/bcategory/bcategorySlice";
+import blogReducer from "../features/blogs/blogSlice";
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +14,7 @@ export const store = configureStore({
     product: productReducer,
     brand: brandReducer,
     pCategory: pCategoryReducer,
+    bCategory: bCategoryReducer,
+    blogs: blogReducer,
   },
 });
--- src/pages/Bloglist.jsx
+++ src/pages/Bloglist.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getBlogs } from "../features/blogs/blogSlice";
 
 const columns = [
   {
@@ -7,29 +12,49 @@ const columns = [
     dataIndex: "key",
   },
   {
-    title: "Name",
+    title: "Title",
     dataIndex: "name",
   },
   {
-    title: "Product",
-    dataIndex: "Product",
+    title: "Category",
+    dataIndex: "category",
   },
   {
-    title: "Status",
-    dataIndex: "status",
+    title: "Action",
+    dataIndex: "action",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Kasun Madushanka ${i}`,
-    product: 32,
-    status: `Horana road, Panadura . ${i}`,
-  });
-}
 
 const Bloglist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBlogs());
+  }, []);
+  const getBlogState = useSelector((state) => state.blogs.blogs);
+  const data1 = [];
+  for (let i = 0; i < getBlogState.length; i++) {
+    data1.push({
+      key: i + 1,
+      name: getBlogState[i].title,
+      category: getBlogState[i].category,
+
+      action: (
+        <>
+          <Link
+            to='/'
+            className=" fs-3 text-danger"
+          >
+            <BiEdit />
+          </Link>
+          <Link
+            className="ms-3 fs-3 text-danger bg-transparent border-0" to='/'
+          >
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
   return (
     <div>
       <h3 className="mb-4 title">Blog List</h3>
