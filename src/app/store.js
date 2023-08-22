6b19483a3e3f29438bf6d03eb38a5dd1098f91be
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-22 17:38:09 +0530

    implement category list API integration

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
 import brandReducer from "../features/brand/brandSlice";
+import pCategoryReducer from "../features/pcategory/pcategorySlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     customer: customerReducer,
     product: productReducer,
     brand: brandReducer,
+    pCategory: pCategoryReducer,
   },
 });
