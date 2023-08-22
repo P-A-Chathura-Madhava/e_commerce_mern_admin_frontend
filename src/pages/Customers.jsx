40abf7939a28f69e920a70cd898337f29922a4e5
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-22 14:35:56 +0530

    implement get all customers API integration

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Table } from "antd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../features/customers/customerSlice";
 
 const columns = [
@@ -11,31 +11,35 @@ const columns = [
   {
     title: "Name",
     dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
   },
   {
-    title: "Product",
-    dataIndex: "Product",
+    title: "Email",
+    dataIndex: "email",
   },
   {
-    title: "Status",
-    dataIndex: "status",
+    title: "Mobile",
+    dataIndex: "mobile",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Kasun Madushanka ${i}`,
-    product: 32,
-    status: `Horana road, Panadura . ${i}`,
-  });
-}
 
 const Customers = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUsers());
   }, []);
+  const customerstate = useSelector((state) => state.customer.customers);
+  const data1 = [];
+  for (let i = 0; i < customerstate.length; i++) {
+    if (customerstate[i].role !== "admin") {
+      data1.push({
+        key: i + 1,
+        name: customerstate[i].firstname + " " + customerstate[i].lastname,
+        email: customerstate[i].email,
+        mobile: customerstate[i].mobile,
+      });
+    }
+  }
   return (
     <div>
       <h3 className="mb-4 title">Customers</h3>
