1bbe1233ca014513c4d9f5ca82e20a40440c91ac
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-22 17:09:24 +0530

    implement brand list API integration

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
 import productReducer from "../features/product/productSlice";
+import brandReducer from "../features/brand/brandSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     customer: customerReducer,
     product: productReducer,
+    brand: brandReducer,
   },
 });
--- src/pages/Brandlist.jsx
+++ src/pages/Brandlist.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../features/brand/brandSlice";
 
 const columns = [
   {
@@ -9,27 +14,37 @@ const columns = [
   {
     title: "Name",
     dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
   },
   {
-    title: "Product",
-    dataIndex: "Product",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
+    title: "Action",
+    dataIndex: "action",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Kasun Madushanka ${i}`,
-    product: 32,
-    status: `Horana road, Panadura . ${i}`,
-  });
-}
 
 const Brandlist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBrands());
+  }, []);
+  const brandState = useSelector((state) => state.brand.brands);
+  const data1 = [];
+  for (let i = 0; i < brandState.length; i++) {
+    data1.push({
+      key: i + 1,
+      name: brandState[i].title,
+      action: (
+        <>
+          <Link to="/" className=" fs-3 text-danger">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
   return (
     <div>
       <h3 className="mb-4 title">Brands</h3>
