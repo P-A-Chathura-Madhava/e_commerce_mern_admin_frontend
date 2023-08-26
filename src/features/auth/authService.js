a7e2ee30bc1ad2c3cc8620d8a83f0a6dc3fa5630
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-26 10:17:02 +0530

    implement Order Page API integration

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/axiosconfig";
-
-const login = async (userData) => {
-  const response = await axios.post(`${base_url}user/admin-login`, userData);
+import { base_url } from "../../utils/base_url";
+const login = async (user) => {
+  const response = await axios.post(`${base_url}user/admin-login`, user);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
   }
@@ -14,10 +13,20 @@ const getOrders = async () => {
 
   return response.data;
 };
+const getOrder = async (id) => {
+  const response = await axios.post(
+    `${base_url}user/getorderbyuser/${id}`,
+    "",
+    config
+  );
+
+  return response.data;
+};
 
 const authService = {
   login,
   getOrders,
+  getOrder,
 };
 
 export default authService;
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,7 +4,6 @@ import authService from "./authService";
 const getUserfromLocalStorage = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
-
 const initialState = {
   user: getUserfromLocalStorage,
   orders: [],
@@ -13,12 +12,11 @@ const initialState = {
   isSuccess: false,
   message: "",
 };
-
 export const login = createAsyncThunk(
-  "auth/admin-login",
-  async (user, thunkAPI) => {
+  "auth/login",
+  async (userData, thunkAPI) => {
     try {
-      return await authService.login(user);
+      return await authService.login(userData);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -35,10 +33,20 @@ export const getOrders = createAsyncThunk(
     }
   }
 );
+export const getOrderByUser = createAsyncThunk(
+  "order/get-order",
+  async (id, thunkAPI) => {
+    try {
+      return await authService.getOrder(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: initialState,
   reducers: {},
   extraReducers: (buildeer) => {
     buildeer
@@ -46,16 +54,17 @@ export const authSlice = createSlice({
         state.isLoading = true;
       })
       .addCase(login.fulfilled, (state, action) => {
+        state.isError = false;
         state.isLoading = false;
         state.isSuccess = true;
         state.user = action.payload;
         state.message = "success";
       })
       .addCase(login.rejected, (state, action) => {
-        state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
-        state.user = null;
+        state.message = action.error;
+        state.isLoading = false;
       })
       .addCase(getOrders.pending, (state) => {
         state.isLoading = true;
@@ -68,6 +77,22 @@ export const authSlice = createSlice({
         state.message = "success";
       })
       .addCase(getOrders.rejected, (state, action) => {
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        state.isLoading = false;
+      })
+      .addCase(getOrderByUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderByUser.fulfilled, (state, action) => {
+        state.isError = false;
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.orderbyuser = action.payload;
+        state.message = "success";
+      })
+      .addCase(getOrderByUser.rejected, (state, action) => {
         state.isError = true;
         state.isSuccess = false;
         state.message = action.error;
